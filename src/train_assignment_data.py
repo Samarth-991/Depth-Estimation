0f1096e3d29a6f8e76e469a3867d45a1c5b80a27
Samarth Tandon <samarthtandon91@gmail.com>
2023-07-03 14:23:24 +0530

    Add files via upload

--- src/train_assignment_data.py
+++ src/train_assignment_data.py
@@ -9,8 +9,8 @@ import torch
 import torch.nn as nn
 import torchvision.utils as vutils
 from torch.utils.data import DataLoader
-
-from data_loader.data_creation import CreateDataset
+from glob import glob
+from data_loader.data_loader_assignment import CreateAssignemntDataset
 from depth_model.loss import ssim
 from depth_model.model import PTModel as Model
 from utils.common import AverageMeter, DepthNorm, colorize
@@ -30,7 +30,7 @@ def fine_tune(pretrined_model):
     layer_names = []
     for idx, (name, param) in enumerate(pretrined_model.named_parameters()):
         layer_names.append(name)
-    # Getting the depth layers for decoder 
+    # Getting the depth layers for decoder
     # reverse layers
     layer_names.reverse()
 
@@ -52,53 +52,42 @@ def fine_tune(pretrined_model):
 
 def train(args):
     logger = create_logger()
-
     data_path = args.path
     model_path = args.outdir
-    args.pretrained = "../model/depthnet.ckpt"
-
-    train_df = pd.read_csv(os.path.join(data_path, 'data/nyu2_train.csv'))
-    train_df.columns = ['RGB_images', 'Depth_images']
-
-    val_df = pd.read_csv(os.path.join(data_path, 'data/nyu2_test.csv'))
-    val_df.columns = ['RGB_images', 'Depth_images']
-
-    ## Sample Data to avoid training for long time
-    train_df = train_df.sample(10000)
-    val_df = val_df.sample(500)
-
-    batch_size = args.batchsize
-    epochs = args.epochs
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    rgb_image_files = [osp.join(data_path, filename) for filename in train_df['RGB_images'].to_list()]
-    depth_image_files = [osp.join(data_path, filename) for filename in train_df['Depth_images'].to_list()]
+    child_rgb_files = glob(data_path + "/*/rgb/*")
 
-    val_rgb_files = [osp.join(data_path, filename) for filename in val_df['RGB_images'].to_list()]
-    val_depth_files = [osp.join(data_path, filename) for filename in val_df['Depth_images'].to_list()]
+    train_sample = int(len(child_rgb_files) * .80)
+    train_rgb_images = child_rgb_files[: train_sample]
+    train_depth_images = [fname.replace('rgb', 'depth') for fname in train_rgb_images]
 
-    train_dataset = CreateDataset(rgb_files=rgb_image_files,
-                                  depth_files=depth_image_files,
-                                  transform=augmentation(),
-                                  process_image=preprocessing(),
-                                  task='Training'
-                                  )
+    valid_rgb_images = child_rgb_files[train_sample:]
+    valid_depth_images = [fname.replace('rgb', 'depth') for fname in valid_rgb_images]
 
-    valid_dataset = CreateDataset(rgb_files=val_rgb_files,
-                                  depth_files=val_depth_files,
-                                  transform=None,
-                                  task='validation',
-                                  process_image=preprocessing())
+    logger.info("Data Size {}".format(len(child_rgb_files)))
+    logger.info(
+        "Images in Training  dataset {}\t Images in Validation Dataset {}".format(len(train_rgb_images),
+                                                                                  len(valid_rgb_images)))
 
-    logger.info("Loaded {} images for training ".format(len(train_dataset)))
-    logger.info("Loaded {} images for validation".format(len(valid_dataset)))
+    train_dataset = CreateAssignemntDataset(rgb_files=train_rgb_images,
+                                            depth_files=train_depth_images,
+                                            transform=None,
+                                            process_image=preprocessing(),
+                                            task='Training'
+                                            )
+    valid_dataset = CreateAssignemntDataset(rgb_files=valid_rgb_images,
+                                            depth_files=valid_depth_images,
+                                            transform=None,
+                                            task='validation',
+                                            process_image=preprocessing())
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+    train_loader = DataLoader(train_dataset, batch_size=args.batchsize, shuffle=True, num_workers=1)
     valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=1)
 
     # Create model
-    model = Model().to(device)
+    model = Model().float().to(device)
 
     if args.pretrained:
         checkpoint = torch.load(args.pretrained)
@@ -112,15 +101,15 @@ def train(args):
     if args.pretrained:
         logger.info("Loading model with pre-trained weights from {}".format(args.pretrained))
         torch.optim.Adam(parameters)
-    # Loss
+        # Loss
     l1_criterion = nn.L1Loss()
 
-    # model Name 
+    # model Name
     model_name = args.model_name + '.ckpt'
 
     # start Training
     epoch_loss = 0.0
-    for epoch in range(epochs):
+    for epoch in range(args.epochs):
         batch_time = AverageMeter()
         losses = AverageMeter()
         N = len(train_loader)
@@ -131,7 +120,7 @@ def train(args):
         for i, sample_image in enumerate(train_loader):
             image = sample_image['image'].to(device)
             depth = sample_image['depth'].to(device)
-            depth_n = DepthNorm(depth)
+            depth_n = depth
             # Predict
             output = model(image)
             l_depth = l1_criterion(output, depth_n)
@@ -149,16 +138,14 @@ def train(args):
             eta = str(datetime.timedelta(seconds=int(batch_time.val * (N - i))))
             # Log progress
             niter = epoch * N + i
-            if i % 10 == 0:
+            if i % 50 == 0:
                 # Print to console
                 logger.info('Epoch: [{0}][{1}/{2}]\t'
                             'Time {batch_time.val:.3f} ({batch_time.sum:.3f})\t'
                             'ETA {eta}\t'
                             'Loss {loss.val:.4f} ({loss.avg:.4f})'
                             .format(epoch + 1, i + 1, N, batch_time=batch_time, loss=losses, eta=eta))
-            if i + 1 % 10 == 0:
-                logger.info("logging progress ")
-                LogProgress(model, valid_loader, niter, device=device)
+
         if epoch == 0:
             epoch_loss = losses.avg
             logger.info("Loss decreased from -inf. to {}".format(losses.avg))
@@ -170,24 +157,7 @@ def train(args):
             torch.save(model.state_dict(), osp.join(model_path, model_name))
             logger.info("saving model checkpoint {}".format(osp.join(model_path, model_name)))
 
-    torch.save(model, os.path.join(model_path, 'depth_model.pt'))
-
-
-def LogProgress(model, val_loader, niter, out_dir='model/runs', device='cpu'):
-    model.eval()
-    sequential = val_loader
-    sample_batch = next(iter(sequential))
-    image = sample_batch['image'].to(device)
-    depth = sample_batch['depth'].to(device)
-
-    output = DepthNorm(model(image))
-    predicted_image = colorize(vutils.make_grid(output.data, nrow=6, normalize=False))
-    print(predicted_image, type(predicted_image))
-    if not out_dir:
-        os.makedirs(out_dir)
-    del image
-    del depth
-    del output
+    torch.save(model, os.path.join(model_path, args.model_name + '.pt'))
 
 
 if __name__ == '__main__':
@@ -196,7 +166,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', default=20, type=int, help='number of total epochs to run')
     parser.add_argument('--lr', '--learning-rate', default=0.0001, type=float, help='initial learning rate')
     parser.add_argument('--batchsize', default=4, type=int, help='batch size')
-    parser.add_argument('--pretrained', default=None, type=bool, help='pretrained network checkpoints ')
+    parser.add_argument('--pretrained', default=None, type=str, help='pretrained network checkpoints ')
     parser.add_argument('--outdir', default='model', type=str, help='output directory for saving model')
     parser.add_argument('--model_name', default='depth_model2', type=str, help='Model name')
     args = parser.parse_args()
